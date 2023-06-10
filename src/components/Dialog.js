206608ad99405af602492852ee86deace9f4e314
Enjae Antonio <enjaeantonio@outlook.com>
2023-06-10 14:41:24 -0500

    removed the switch case for better time complexity

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,18 +1,18 @@
 function Dialog({ open, onClose }) {
     return (
-      <section className="fixed top-20 flex items-center justify-center">
-        {open ? (
+      <section className={`flex justify-center items-center ${open ? "absolute h-screen w-screen" : ""}`}>
+      {open && (
           <div>
             <img
               className="w-40"
               src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
               alt="logo"
             />
-            <button className="text-white bg-red-500 rounded-md py-1 mt-2 flex w-40 flex items-center justify-center" onClick={onClose}>
+            <button className="text-white bg-red-500 rounded-md py-1 mt-2 w-40 flex items-center justify-center" onClick={onClose}>
                 Close Image
             </button>
           </div>
-        ) : null}
+        )}
         <button className="text-white fixed bottom-5 right-5 w-40 rounded-md bg-custom-blue py-1"onClick={onClose}>
           {open ? "Close Dialog" : "Open Dialog"}
         </button>
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,41 +1,24 @@
 import { useState } from "react"
 import Star from "./Star"
 
-function StarRating({totalStars = 5}) {
+function StarRating() {
     const [starsSelected, setStarsSelected] = useState(0);
-    const arrayOfStars = [...Array(totalStars)]
-
-    const feedback = (index) => {
-        switch(index){
-            case 1:
-                return "Oh no!";
-            case 2:
-                return "Not bad!";
-            case 3: 
-                return "Nice!";
-            case 4:
-                return "Very Good!";
-            case 5:
-                return "Excellent!";
-            default:
-                return "";
-        }
-    }
-    
+    const feedback = ["", "Oh no!", "Not Bad", "Good Job", "Very Good!", "Amazing!"]
+    const arrayOfStars = Array(5).fill().map((star, index) =>{
+        return (
+        <Star
+            key = {index}
+            color = {starsSelected > index}
+            onSelect={() => setStarsSelected(index + 1)}
+        />
+        )
+    })
   return (
     <section className="flex flex-col text-center justify-center">
         <div className="cursor-pointer flex justify-center text-5xl">
-        {arrayOfStars.map((star, index) =>{
-            return (
-            <Star
-                key = {index}
-                color = {starsSelected > index}
-                onSelect={() => setStarsSelected(index + 1)}
-            />
-            )
-        })}
+        {arrayOfStars}
         </div>
-        <h2 className="text-white text-1xl mt-2">{starsSelected ? feedback(starsSelected) : "Give us a rating!"}</h2>
+        <h2 className="text-white text-1xl mt-2">{starsSelected ? feedback[starsSelected] : "Give your feedback!"}</h2>
     </section>
   )
 }
